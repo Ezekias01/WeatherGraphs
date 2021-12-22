45034251d26549911ab3a87db0e1cfb3dc46af4d
Piz01 <95301079+Piz01@users.noreply.github.com>
2021-12-22 23:38:55 +0100

    Add files via upload
    Upload of class "Punto"

--- demo.springboot.application/src/main/java/com/example/demo/springboot/application/model/Punto.java
+++ demo.springboot.application/src/main/java/com/example/demo/springboot/application/model/Punto.java
@@ -3,10 +3,12 @@ package com.example.demo.springboot.application.model;
 public class Punto {
 	private double lat;
 	private double longit;
+	private Misura misura;
 	
-	public Punto(double lat,double longit) {
+	public Punto(double lat,double longit,Misura misura) {
 		this.lat=lat;
 		this.longit=longit;
+		this.misura=misura;
 	}
 
 	public double getLat() {
@@ -17,4 +19,9 @@ public class Punto {
 	public double getLongit() {
 		return longit;
 	}
+
+	public Misura getMisura() {
+		return misura;
+	}
+	
 }
